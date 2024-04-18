7e57320d719d6196b66856fb0621d896754c2f2c
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2024-04-18 10:07:36 -0600

    added files and folder

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+  content: ['./*.html'],
+  screens: {
+    sm: '480',
+    md: '768px',
+    lg:'976px',
+    xl: '1440px'
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Barlow Semi Condensed', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
